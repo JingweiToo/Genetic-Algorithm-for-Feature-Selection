0fc2ed55f8ee0d53555e5f490a9f62628f4df633
Jingwei Too <30698779+JingweiToo@users.noreply.github.com>
2020-12-19 21:48:27 +0800

    Add files via upload

--- Main.m
+++ Main.m
@@ -1,59 +1,72 @@
-%-------------------------------------------------------------------------%
-%  Genetic Algorithm (GA) source codes demo version                       %
-%                                                                         %
-%  Programmer: Jingwei Too                                                %
-%                                                                         %
-%  E-Mail: jamesjames868@gmail.com                                        %
-%-------------------------------------------------------------------------%
+%-------------------------------------------------------------------%
+%  Genetic Algorithm (GA) demo version                              %
+%-------------------------------------------------------------------%
 
 
-%---Input------------------------------------------------------------------
-% feat:  feature vector (instances x features)
-% label: labelling
-% N:     Number of chromosomes
-% T:     Maximum number of generations
-% CR:    Crossover rate
-% MR:    Mutation rates
-%---Output-----------------------------------------------------------------
-% sFeat: Selected features (instances x features)
-% Sf:    Selected feature index
-% Nf:    Number of selected features
-% curve: Convergence curve
-%--------------------------------------------------------------------------
+%---Input------------------------------------------------------------
+% feat     : feature vector (instances x features)
+% label    : label vector (instance x 1)
+% N        : Number of chromosomes
+% max_Iter : Maximum number of generations
+% CR       : Crossover rate
+% MR       : Mutation rates
+
+%---Output-----------------------------------------------------------
+% sFeat    : Selected features (instances x features)
+% Sf       : Selected feature index
+% Nf       : Number of selected features
+% curve    : Convergence curve
+%--------------------------------------------------------------------
 
 
 %% Genetic Algorithm (version 1) 
 clc, clear, close 
 % Benchmark data set 
 load ionosphere.mat; 
+
 % Set 20% data as validation set
-ho=0.2; 
+ho = 0.2; 
 % Hold-out method
-HO=cvpartition(label,'HoldOut',ho,'Stratify',false);
+HO = cvpartition(label,'HoldOut',ho,'Stratify',false);
+
 % Parameter setting
-N=10; T=100; CR=0.8; MR=0.3; 
+N        = 10;
+max_Iter = 100;
+CR       = 0.8;
+MR       = 0.3; 
 % Genetic Algorithm
-[sFeat,Sf,Nf,curve]=jGA1(feat,label,N,T,CR,MR,HO); 
+[sFeat,Sf,Nf,curve] = jGA1(feat,label,N,max_Iter,CR,MR,HO); 
+
 % Plot convergence curve
-figure(); plot(1:T,curve); xlabel('Number of generations');
-ylabel('Fitness Value'); title('GA'); grid on;
+plot(1:max_Iter,curve);
+xlabel('Number of generations');
+ylabel('Fitness Value'); 
+title('GA'); grid on;
 
 
 %% Genetic Algorithm (version 2) 
 clc, clear, close 
 % Benchmark data set 
 load ionosphere.mat; 
+
 % Set 20% data as validation set
-ho=0.2; 
+ho = 0.2; 
 % Hold-out method
-HO=cvpartition(label,'HoldOut',ho,'Stratify',false);
+HO = cvpartition(label,'HoldOut',ho,'Stratify',false);
+
 % Parameter setting
-N=10; T=100; CR=0.6; MR=0.001; 
+N        = 10;
+max_Iter = 100; 
+CR       = 0.6; 
+MR       = 0.001; 
 % Genetic Algorithm
-[sFeat,Sf,Nf,curve]=jGA2(feat,label,N,T,CR,MR,HO);
+[sFeat,Sf,Nf,curve] = jGA2(feat,label,N,max_Iter,CR,MR,HO);
+
 % Plot convergence curve
-figure(); plot(1:T,curve); xlabel('Number of generations');
-ylabel('Fitness Value'); title('GA'); grid on;
+plot(1:max_Iter,curve);
+xlabel('Number of generations');
+ylabel('Fitness Value'); 
+title('GA'); grid on;
 
 
 
--- jGA1.m
+++ jGA1.m
@@ -1,65 +1,87 @@
-function [sFeat,Sf,Nf,curve]=jGA1(feat,label,N,T,CR,MR,HO)
+function [sFeat,Sf,Nf,curve] = jGA1(feat,label,N,max_Iter,CR,MR,HO)
 
-fun=@jFitnessFunction; 
-D=size(feat,2); X=zeros(N,D); 
-for i=1:N
-  for d=1:D
+fun = @jFitnessFunction; 
+dim = size(feat,2);
+X   = zeros(N,dim); 
+for i = 1:N
+  for d = 1:dim
     if rand() > 0.5
-      X(i,d)=1;
+      X(i,d) = 1;
     end
   end
 end
-fit=zeros(1,N); fitG=inf;
-for i=1:N
-  fit(i)=fun(feat,label,X(i,:),HO);
+fit  = zeros(1,N); 
+fitG = inf;
+for i = 1:N
+  fit(i) = fun(feat,label,X(i,:),HO);
   if fit(i) < fitG
-    fitG=fit(i); Xgb=X(i,:);
+    fitG = fit(i); 
+    Xgb  = X(i,:);
   end
 end
-curve=inf; t=1; 
-%---Generations start------------------------------------------------------
-while t <= T
-  Ifit=1-fit; Prob=Ifit/sum(Ifit);
-  X1=zeros(1,D); X2=zeros(1,D); z=1;
-  for i=1:N
+
+curve = inf;
+t = 1; 
+%---Generations start------------------------------------------------
+while t <= max_Iter
+  Ifit = 1 - fit; 
+  prob = Ifit / sum(Ifit);
+  X1   = zeros(1,dim);
+  X2   = zeros(1,dim);
+  z    = 1;
+  for i = 1:N
     if rand() < CR
-      k1=jRouletteWheelSelection(Prob);
-      k2=jRouletteWheelSelection(Prob);
-      P1=X(k1,:); P2=X(k2,:);
-      ind=randi([1,D-1]);
-      X1(z,:)=[P1(1:ind),P2(ind+1:D)]; 
-      X2(z,:)=[P2(1:ind),P1(ind+1:D)]; z=z+1;
+      k1  = jRouletteWheelSelection(prob);
+      k2  = jRouletteWheelSelection(prob);
+      P1  = X(k1,:);
+      P2  = X(k2,:);
+      ind = randi([1, dim - 1]);
+      X1(z,:) = [P1(1 : ind), P2(ind + 1 : dim)]; 
+      X2(z,:) = [P2(1 : ind), P1(ind + 1 : dim)];
+      z = z + 1;
     end
   end
-  Xnew=[X1;X2]; Nc=size(Xnew,1); Fnew=zeros(1,Nc);
-  for i=1:Nc
-    for d=1:D
+  Xnew = [X1; X2];
+  Nc   = size(Xnew,1); 
+  Fnew = zeros(1,Nc);
+  for i = 1:Nc
+    for d = 1:dim
       if rand() <= MR
-        Xnew(i,d)=1-Xnew(i,d);
+        Xnew(i,d) = 1 - Xnew(i,d);
       end
     end
   end 
-  for i=1:Nc
-    Fnew(i)=fun(feat,label,Xnew(i,:),HO);
+  for i = 1:Nc
+    Fnew(i) = fun(feat,label,Xnew(i,:),HO);
     if Fnew(i) < fitG
-      Xgb=Xnew(i,:); fitG=Fnew(i);
+      Xgb  = Xnew(i,:); 
+      fitG = Fnew(i);
     end
   end 
-  XX=[X;Xnew]; FF=[fit,Fnew]; 
-  [FF,idx]=sort(FF,'ascend'); X=XX(idx(1:N),:); fit=FF(1:N); 
-  curve(t)=fitG; 
+  XX  = [X; Xnew]; 
+  FF  = [fit, Fnew]; 
+  [FF, idx] = sort(FF,'ascend');
+  X   = XX(idx(1:N),:); 
+  fit = FF(1:N);
+  
+  curve(t) = fitG; 
   fprintf('\nIteration %d Best (GA)= %f',t,curve(t))
-  t=t+1;
+  t = t + 1;
 end
-Pos=1:D; Sf=Pos(Xgb==1); Nf=length(Sf); sFeat=feat(:,Sf); 
+Pos   = 1:dim; 
+Sf    = Pos(Xgb == 1); 
+Nf    = length(Sf); 
+sFeat = feat(:,Sf); 
 end
 
 
-function Route=jRouletteWheelSelection(Prob)
-C=cumsum(Prob); P=rand();
-for i=1:length(C)
+function Index = jRouletteWheelSelection(prob)
+C = cumsum(prob); 
+P = rand();
+for i = 1:length(C)
 	if C(i) > P
-    Route=i; break;
+    Index = i;
+    break;
   end
 end
 end
